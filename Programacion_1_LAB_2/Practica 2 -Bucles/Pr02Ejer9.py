cdd0be431b30e6f48c0d629fa829387a0d4b0b6e
JesusJMUJI <al427616@uji.es>
2022-10-11 17:49:51 +0200

    Finalización Ej9

--- Programacion_1_LAB_2/Practica 2 -Bucles/Pr02Ejer9.py
+++ Programacion_1_LAB_2/Practica 2 -Bucles/Pr02Ejer9.py
@@ -3,25 +3,27 @@ from turtle import *
 pantalla = Screen()
 pantalla.setup(800,800)
 tortuga = Turtle()
+tortuga.speed(speed=10)
 
-numDibujos = int(input("Numero de dibujos: "))
 grosor = randint(1,20)
 redvar = random()
 greenvar = random()
 bluevar = random()
+
 userInput = True
 
-while 
-
-for x in range(numDibujos):
-    tortuga.pensize(grosor)
-    tortuga.pencolor(redvar,greenvar,bluevar)
-    tortuga.forward(randint(1,100))
-    tortuga.left(randint(1,360))
-    redvar = random()
-    greenvar = random()
-    bluevar = random()
-    grosor = randint(1,20)
-
-
+while userInput:
+    numDibujos = int(input("Numero de dibujos: "))
+    for x in range(numDibujos):
+        tortuga.pensize(grosor)
+        tortuga.pencolor(redvar,greenvar,bluevar)
+        tortuga.forward(randint(1,100))
+        tortuga.left(randint(1,360))
+        redvar = random()
+        greenvar = random()
+        bluevar = random()
+        grosor = randint(1,20)
+    userInput = input("Desea continuar? (y/n): ")
+    if userInput == "n":
+        break
 pantalla.exitonclick()
--- Programacion_1_LAB_2/Practica 2 -Bucles/Pr02ejer10.py
+++ Programacion_1_LAB_2/Practica 2 -Bucles/Pr02ejer10.py
@@ -6,6 +6,7 @@ screen.setup(1000,1000)
 
 lados = int(input("Lados del polígono: "))
 longitud = int(input("Longitud del lado: "))
+radioDecor = int(input("Radio del decorado: "))
 
 for x in range(lados):
     tortuga.forward(longitud)
