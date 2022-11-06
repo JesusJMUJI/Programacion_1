af0f3b07644f12ae8cc8a07b54b73a9c3022f83d
JesusJMUJI <al427616@uji.es>
2022-11-07 00:29:06 +0100

    Update Ej4_2.py

--- Programacion_1_LAB_2/Practica 3 - Cadenas/Ej4_2.py
+++ Programacion_1_LAB_2/Practica 3 - Cadenas/Ej4_2.py
@@ -2,3 +2,35 @@ from random import randint
 print("Práctica 3 - Ejercicio 4")
 print("")
 
+print("Juego de Toros y Vacas")
+print("Dime un numero entre 1 a 9 y averigua el numero aleatorio")
+inputCadena = input("¿Cual es tu intento?: ")
+codigoRandom = ""
+contadorIndice = 0
+coincidenciasToros = 0
+coincidenciasVacas = 0
+ganador = False
+x = 0
+y = 0
+
+
+for numero in range(0,4):
+    codigoRandom = codigoRandom + str(randint(1,9))
+print(codigoRandom)
+
+for i in inputCadena:
+    x+=1
+    for j in codigoRandom:
+        y += 1
+        if i == j:
+            if x == y:
+                coincidenciasToros += 1
+            else:
+                coincidenciasVacas += 1
+
+print("Toros: " + str(coincidenciasToros) + "Vacas: " + str(coincidenciasToros))
+    
+
+
+
+
