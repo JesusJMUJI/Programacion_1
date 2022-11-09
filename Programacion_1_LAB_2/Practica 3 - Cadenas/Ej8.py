e88b2c05cbe9ded819ba5179d781ee36e507ee28
JesusJMUJI <al427616@uji.es>
2022-11-09 09:18:05 +0100

    Ej8 en el tren

--- Programacion_1_LAB_2/Practica 3 - Cadenas/Ej8.py
+++ Programacion_1_LAB_2/Practica 3 - Cadenas/Ej8.py
@@ -4,7 +4,27 @@ print("")
 n = int(input("Dime como de grande es la matriz (n x n): "))
 M = []
 for i in range(n):
-	M.append([0]* n)
-	
+	M.append([0] * n)
+
+for i in range(n):
+	for j in range(n):
+		if i == j:
+			M[i][j] = 0
+		if i != j:
+			M[i][j] = 1
+			if i < j:
+				M[i][j] = 2
+				if i == j + 1:
+					M[i][j] = -1
+			if i > j:
+				M[i][j] = -2
+				if i == j + 1:
+					M[i][j] = -1
+
+
+for i in range(n):
+	for j in range(n):
+		print(M[i][j], end=' ')
+	print()
 
 print(M)
