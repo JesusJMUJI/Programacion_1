491d62b471974058e32ede68da4a87db3cce0b2b
Jesus_JM_UJI <al427616@uji.es>
2022-12-13 19:26:29 +0100

    Update prac05ej04ej06.py

--- Programacion_1_LAB_2/Practica_5_PyGame/ficheros_practica_5/prac05ej04ej06.py
+++ Programacion_1_LAB_2/Practica_5_PyGame/ficheros_practica_5/prac05ej04ej06.py
@@ -25,110 +25,145 @@ COCULTA='-'
 
 # Función para crear el tablero visible
 def crear_tablero_visible(filas, columnas):
-	# El código de la función debe ir aquí
-	for i in filas:
-		tvisible.append([C0].append(columnas))
-
+    # El código de la función debe ir aquí
+    tvisible = []
+    for i in range(filas):
+        tvisible.append([C0] * (columnas))
+    return tvisible
 # Función para crear el tablero oculto
 def crear_tablero_oculto(filas, columnas):
-	# El código de la función debe ir aquí
-	for i in filas:
-		toculto.append([COCULTA].append(columnas))
+    # El código de la función debe ir aquí
+    toculto = []
+    for f in range(filas):
+        toculto.append([COCULTA] * columnas)
+    return toculto
 
 # Función para poner las bombas en el tablero oculto
 def poner_bombas_tablero_oculto(toculto, bombas):
-	# El código de la función debe ir aquí
-	while bombas > # NUMERO DE BOMBAS SEGUN EL NIVEL DE DIFICULTAD:
-		for fila in range(toculto):
-			for columna in range(toculto):
-				if randint(0, 1) == 1:
-					columna = CBOMBA
-			if randint(0, 1) == 1:
-				fila = CBOMBA
-
+    # El código de la función debe ir aquí
+    i = 0
+    while i < bombas:
+        fila = randint(0,len(toculto)-1)
+        columna = randint(0,len(toculto)-1)
+        
+        if toculto[fila][columna] != CBOMBA:
+            toculto[fila][columna] = CBOMBA
+            i+=1
 # Función para poner la información en el tablero oculto
 def poner_info_tablero_oculto(toculto):
-	filas = len(toculto)
-	columnas = len(toculto[0])
+    filas = len(toculto)
+    columnas = len(toculto[0])
 
-	for f in range(filas):
-		for c in range(columnas):
-			if toculto[f][c] != CBOMBA:
-				adyacentes = []
-				## Añadir a adyacentes las tres casillas superiores, si las hay
-				## (solo cuando f>0, teniendo en cuenta que para la superior izda.
-				## c>0, y que para la superior dcha. c<columnas-1)
-				if f > 0:
-					if c > 0:
-						adyacentes.append(toculto[f-1][c-1])
-					adyacentes.append(toculto[f-1][c])
-					if c < columnas-1:
-						adyacentes.append(toculto[f-1][c+1])
+    for f in range(filas):
+        for c in range(columnas):
+            if toculto[f][c] != CBOMBA:
+                adyacentes = []
+                cont_bombas = 0
+                ## Añadir a adyacentes las tres casillas superiores, si las hay
+                ## (solo cuando f>0, teniendo en cuenta que para la superior izda.
+                ## c>0, y que para la superior dcha. c<columnas-1)
+                if f > 0:
+                    if c > 0:
+                        adyacentes.append(toculto[f-1][c-1])
+                    adyacentes.append(toculto[f-1][c])
+                    if c < columnas-1:
+                        adyacentes.append(toculto[f-1][c+1])
 
 
-				## Añadir a adyacentes las casillas a la izda. y la dcha., si las
-				## hay (teniendo en cuenta que para la izda. c>0, y que para la
-				## dcha c<columnas-1)
-				if c > 0:
-					adyacentes.append(toculto[f][c-1])
-				if c < columnas-1:
-					adyacentes.append(toculto[f][c+1])
-				## Añadir a adyacentes las tres casillas inferiores, si las hay
-				## (solo cuando f<filas-1, teniendo en cuenta que para la inferior
-				## izda. c>0, y que para la inferior dcha. c<columnas-1)
-				if f < filas-1:
-					if c > 0:
-						adyacentes.append(toculto[f+1][c-1])
-					adyacentes.append(toculto[f+1][c])
-					if c < columnas-1:
-						adyacentes.append(toculto[f+1][c+1])
+                ## Añadir a adyacentes las casillas a la izda. y la dcha., si las
+                ## hay (teniendo en cuenta que para la izda. c>0, y que para la
+                ## dcha c<columnas-1)
+                if c > 0:
+                    adyacentes.append(toculto[f][c-1])
+                    
+                if c < columnas-1:
+                    adyacentes.append(toculto[f][c+1])
+                    
+                ## Añadir a adyacentes las tres casillas inferiores, si las hay
+                ## (solo cuando f<filas-1, teniendo en cuenta que para la inferior
+                ## izda. c>0, y que para la inferior dcha. c<columnas-1)
+                if f < filas-1:
+                    if c > 0:
+                        adyacentes.append(toculto[f+1][c-1])
+                    adyacentes.append(toculto[f+1][c])
+                    if c < columnas-1:
+                        adyacentes.append(toculto[f+1][c+1])
 
-				toculto[f][c] = str(adyacentes.count(CBOMBA))
-
+                # Recorrer adyacentes
+                for elem in adyacentes:
+                    if elem == CBOMBA:
+                        cont_bombas += 1
+                        
+                # Poner bombas según el numero de bombas adyacentes
+                if cont_bombas == 0:
+                    toculto[f][c] = C0
+                elif cont_bombas == 1:
+                    toculto[f][c] = C1
+                elif cont_bombas == 2:
+                    toculto[f][c] = C2
+                elif cont_bombas == 3:
+                    toculto[f][c] = C3
+                elif cont_bombas == 4:
+                    toculto[f][c] = C4
+                elif cont_bombas == 5:
+                    toculto[f][c] = C5
+                elif cont_bombas == 6:
+                    toculto[f][c] = C6
+                elif cont_bombas == 7:
+                    toculto[f][c] = C7
+                elif cont_bombas == 8:
+                    toculto[f][c] = C8
 
 def imprimir_tablero(tablero):
-	# El código de la función debe ir aquí
-	for i in tablero:
-		print(i)
+    # El código de la función debe ir aquí
+    for i in range(len(tablero)):
+        for j in range(len(tablero)):
+            print(tablero[i][j], end= ' ')
+        print()
+    
+#def tablero_visible_destapar(tvisible, toculto, fila, columna):
+    # El código de la función debe ir aquí
 
-def tablero_visible_destapar(tvisible, toculto, fila, columna):
-	# El código de la función debe ir aquí
+#def tablero_visible_marcar(tvisible, fila, columna, onoff):
+    # El código de la función debe ir aquí
 
 
-
-def tablero_visible_marcar(tvisible, fila, columna, onoff):
-	# El código de la función debe ir aquí
-
-
-def comprobar_tablero_visible(tvisible, toculto, bombas):
-	# El código de la función debe ir aquí
-
+#def comprobar_tablero_visible(tvisible, toculto, bombas):
+    # El código de la función debe ir aquí
+    
 
 def menu_buscaminas():
-	# El código de la función debe ir aquí
+    # El código de la función debe ir aquí
 
-	opcion = 0
-	while opcion < 0 or opcion > 5:
-		print("1. Destapar casilla")
-		print("2. Marcar casilla")
-		print("3. Comprobar tablero")
-		print("4. Bombas por detectar")
-		print("5. Salir")
-		opcion = int(input("Opción: "))
-	return opcion
+    opcion = 0
+    while opcion < 0 or opcion > 5:
+        print("1. Destapar casilla")
+        print("2. Marcar casilla")
+        print("3. Comprobar tablero")
+        print("4. Bombas por detectar")
+        print("5. Salir")
+        opcion = int(input("Opción: "))
+    return opcion
 
 def main():
-	# El código de la función debe ir aquí
-	imprimir_tablero(toculto)
-	imprimir_tablero(tvisible)
-
-	menu_buscaminas()
+    # El código de la función debe ir aquí
+    tvisible = crear_tablero_visible(5, 5)
+    toculto = crear_tablero_oculto(5, 5)
+    
+    imprimir_tablero(tvisible)
+    imprimir_tablero(toculto)
+    poner_bombas_tablero_oculto(toculto, 5)
+    poner_info_tablero_oculto(toculto)
+    imprimir_tablero(toculto)
+    
+    
+    #menu_buscaminas()
 
 
 
 # –- Programa principal –-
 # Ejecutar el test solo al ejecutar el fichero (y no al importarlo)
 if __name__ == "__main__":
-	# El código para probar todas las funciones anteriores debe ir aquí
-	# ...
-	main()
+    # El código para probar todas las funciones anteriores debe ir aquí
+    # ...
+    main()
